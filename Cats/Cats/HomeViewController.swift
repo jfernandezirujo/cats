d0857821865c72d10fa4f91f992cd868377b649e
jfernandezirujo <jfernandezirujo@gmail.com>
2020-12-16 18:58:23 -0300

    home screen (app title, image and tand tab bar item)

---
+++ Cats/Cats/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Cats
+//
+//  Created by Josefina Perez on 16/12/2020.
+//  Copyright © 2020 Julieta Fer. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet var lblCat: UILabel!
+    @IBOutlet var imgCat: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        config()
+
+    }
+
+    func config() {
+        lblCat.text = "CATS!"
+        lblCat.textColor = .black
+        
+        imgCat.image = UIImage(named: "Image")
+        
+        tabBarItem.title = "Home"
+        tabBarItem.image = UIImage(systemName: "house")
+        
+        
+    }
+
+}
+
